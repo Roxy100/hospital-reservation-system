4a05e27f6b4a29d41d2b366ca3dff42f7458e6f8
Roxy100 <misoamy16@gmail.com>
2022-10-12 17:28:50 +0900

    UI : react-datepicker 설치 및 Input 해당날짜 선택 기능구현

--- src/Pages/Schedule.js
+++ src/Pages/Schedule.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import { ko } from 'date-fns/esm/locale';
+import 'react-datepicker/dist/react-datepicker.css';
+import styled from 'styled-components';
 
 const Schedule = () => {
-  return <div>병원 예약 가능 목록</div>;
+  const [checkInDate, setCheckInDate] = useState(new Date());
+
+  return (
+    <StyledSchedule>
+      <div className="schedule_container">
+        <DatePicker
+          withPortal
+          locale={ko}
+          dateFormat="yyyy.MM.dd(eee)"
+          minDate={new Date()}
+          selected={checkInDate}
+          onChange={date => setCheckInDate(date)}
+        />
+      </div>
+    </StyledSchedule>
+  );
 };
+
+const StyledSchedule = styled.div`
+  .schedule_container {
+    width: 100%;
+    margin: 100px 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 export default Schedule;
