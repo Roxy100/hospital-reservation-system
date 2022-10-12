7e959be8abf16e0f778583b502d659ad2c87e7bd
Yim Ji Young <87253461+Roxy100@users.noreply.github.com>
2022-10-12 09:20:18 +0900

    UI : 각각 페이지와 라우터 설정 (#1)
    UI : 각각 페이지와 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
+import { Link } from 'react-router-dom';
+import './App.css';
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="app_container">
+      <div className="app_links">
+        <div>
+          <Link
+            to="/schedule"
+            style={{
+              textDecoration: 'none',
+              fontWeight: 'bolder',
+              color: '#000000',
+            }}
+          >
+            병원 예약 가능 목록
+          </Link>
+        </div>
+        <div>
+          <Link
+            to="/register"
+            style={{
+              textDecoration: 'none',
+              fontWeight: 'bolder',
+              color: '#000000',
+            }}
+          >
+            병원 예약 등록
+          </Link>
+        </div>
+        <div>
+          <Link
+            to="/reservation"
+            style={{
+              textDecoration: 'none',
+              fontWeight: 'bolder',
+              color: '#000000',
+            }}
+          >
+            병원 예약 내역 조회
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
+import Schedule from './Pages/Schedule';
+import Register from './Pages/Register';
+import Reservation from './Pages/Reservation';
 
 import GlobalStyles from './Styles/GlobalStyles';
 
@@ -9,7 +12,10 @@ function Router() {
     <BrowserRouter>
       <GlobalStyles />
       <Routes>
-        <Route path='/' element={<App />} />
+        <Route path="/" element={<App />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/reservation" element={<Reservation />} />
       </Routes>
     </BrowserRouter>
   );
