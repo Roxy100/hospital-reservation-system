8995e518139dec8bacaead5c5027d505b53806f7
Yim Ji Young <87253461+Roxy100@users.noreply.github.com>
2022-10-12 23:03:33 +0900

    UI , Feature : 예약 가능 목록 목데이타 임시 저장 및 예약 가능 기능 구현 중간저장 (#2)
    * UI : react-datepicker 설치 및 Input 해당날짜 선택 기능구현
    
    * UI, Feature: 예약가능목록 목데이타 임시 저장 및 UI 구현

--- src/Pages/Schedule.js
+++ src/Pages/Schedule.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import { ko } from 'date-fns/esm/locale';
+import 'react-datepicker/dist/react-datepicker.css';
+import styled from 'styled-components';
+
+import ScheduleList from '../Components/ScheduleList';
 
 const Schedule = () => {
-  return <div>병원 예약 가능 목록</div>;
+  const [checkInDate, setCheckInDate] = useState(new Date());
+
+  return (
+    <StyledSchedule>
+      <div className="schedule_container">
+        <DatePicker
+          withPortal
+          locale={ko}
+          dateFormat="yyyy.MM.dd(eee)"
+          minDate={new Date()}
+          selected={checkInDate}
+          onChange={date => setCheckInDate(date)}
+        />
+        <ScheduleList checkInDate={checkInDate} />
+      </div>
+    </StyledSchedule>
+  );
 };
+
+const StyledSchedule = styled.div`
+  .schedule_container {
+    width: 100%;
+    margin: 100px 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 export default Schedule;
