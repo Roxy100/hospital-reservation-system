86b89bf9dc98fcbd51fe34d1a8db4f5ea6ecfa9d
Roxy100 <misoamy16@gmail.com>
2022-10-17 21:11:08 +0900

    UI, Feature: 달력 라이브러리 커스텀 설정 및 해당날짜만 선택가능하게 구현

--- src/Pages/Schedule.js
+++ src/Pages/Schedule.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
 import { ko } from 'date-fns/esm/locale';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -8,6 +9,27 @@ import ScheduleList from '../Components/ScheduleList';
 
 const Schedule = () => {
   const [checkInDate, setCheckInDate] = useState(new Date());
+  const navigate = useNavigate();
+
+  // 이 날짜들만 선택할 수 있음.
+  const includeDates = [
+    new Date(2022, 9, 21),
+    new Date(2022, 9, 24),
+    new Date(2022, 9, 25),
+    new Date(2022, 9, 26),
+    new Date(2022, 9, 27),
+    new Date(2022, 9, 28),
+    new Date(2022, 9, 30),
+    new Date(2022, 9, 31),
+    new Date(2022, 10, 1),
+    new Date(2022, 10, 2),
+  ];
+
+  const CustomInput = ({ value, onClick }) => (
+    <button className="custom_input" onClick={onClick}>
+      {value}
+    </button>
+  );
 
   return (
     <StyledSchedule>
@@ -19,7 +41,18 @@ const Schedule = () => {
           minDate={new Date()}
           selected={checkInDate}
           onChange={date => setCheckInDate(date)}
-        />
+          includeDates={includeDates}
+          customInput={<CustomInput />}
+        >
+          <div className="notification">표시된 날짜만 예약이 가능합니다.</div>
+        </DatePicker>
+        <button
+          onClick={() => {
+            navigate('/register');
+          }}
+        >
+          등록하러가기
+        </button>
         <ScheduleList checkInDate={checkInDate} />
       </div>
     </StyledSchedule>
@@ -34,6 +67,37 @@ const StyledSchedule = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    .custom_input {
+      width: 200px;
+      padding: 10px;
+      font-size: 20px;
+      font-weight: 600;
+      background-color: #e3f2fd;
+      border: 1px solid #90caf9;
+      border-radius: 11px;
+    }
+
+    .notification {
+      padding: 20px;
+      color: #1976d2;
+      font-size: 18px;
+      font-weight: 800;
+    }
+
+    button {
+      margin: 20px 0;
+      padding: 10px;
+      font-size: 20px;
+      font-weight: 700;
+      color: black;
+      background-color: #64b5f6;
+      border: 3px solid black;
+      border-radius: 20px;
+      &:hover {
+        cursor: pointer;
+      }
+    }
   }
 `;
 
